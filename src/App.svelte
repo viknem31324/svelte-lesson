e6989449b5d9440c0f911c312b27dd8a211585cd
v.nemov <v.nemov@kokoc.tech>
2023-03-24 17:26:46 +0300

    feat(init): настройка проекта

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	import WordComponent from "./WordComponent.svelte";
+	export let response = false;
+
+	export let wordbook = [
+		{word: 'apple', translation: 'яблоко', incorrect: 0},
+		{word: 'work', translation: 'работа', incorrect: 0},
+		{word: 'walk', translation: 'идти', incorrect: 0},
+		{word: 'read', translation: 'читать', incorrect: 0},
+		{word: 'book', translation: 'книга', incorrect: 0},
+		{word: 'spring', translation: 'весна', incorrect: 0},
+		{word: 'lesson', translation: 'урок', incorrect: 0},
+		{word: 'promise', translation: 'обещание', incorrect: 0},
+		{word: 'component', translation: 'компонент', incorrect: 0},
+		{word: 'language', translation: 'язык', incorrect: 0},
+		{word: 'pen', translation: 'ручка', incorrect: 0},
+		{word: 'phone', translation: 'телефон', incorrect: 0},
+		{word: 'task', translation: 'задача', incorrect: 0},
+	];
+
+	export let word = wordbook[0];
+
+	function randomWord() {
+		word =  wordbook[Math.ceil(Math.random() * 10)];
+	}
+
+	function toggleWord() {
+		response = !response;
+	}
+
+	$: getWord = response ? word.word : word.translation;
+
+</script>
+
+<main>
+	<h1>Hello {getWord}!</h1>
+	<WordComponent on:click={toggleWord} word={getWord} />
+	<button on:click={randomWord}>Дальше</button>
+</main>
+
+<style>
+main {
+	height: 100%;
+}
+</style>
---
+++ src/WordComponent.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let word;
+</script>
+
+<div class="word" on:click>
+  <h3 class="word__title">{ word }</h3>
+</div>
+
+<style lang="scss">
+.word {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  height: 100%;
+  max-height: 200px;
+  border-radius: 20px;
+  background-color: rgb(238, 210, 54);
+}
+
+.word__title {
+    font-size: 20px;
+    color: white;
+  }
+</style>
